bc138bacd8d1a5d5d3809a4ccbbddea5e89cd2e8
Karaba21 <agustin.karabajich@gmail.com>
2023-05-08 20:57:51 -0300

    finalizado2

--- src/Library/IRecipePrinter.cs
+++ src/Library/IRecipePrinter.cs
@@ -10,3 +10,6 @@ namespace Full_GRASP_And_SOLID.Library
     }
 
 }
+
+//en vez de usar la clase AllInOnePrinter cree la clase FilePrinter, ConsolePrinter y la interfaz IRecipePrinter
+//aplicando Polimorfismo
